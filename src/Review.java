9981994d5bf54b961b96aaf6ced2bb90b164fbb9
niveditasankar <dita.sankar@gmail.com>
2016-04-04 21:08:01 -0400

    created Review and User objects

--- src/Review.java
+++ src/Review.java
@@ -1,12 +1,37 @@
 public class Review {
 
-	User author;
-	String text;
-	double ranking;
+	private User author;
+	private String text;
+	private double ranking;
 
 	public Review(User user, String text) {
 		this.author = user;
 		this.text = text;
 		ranking = 0.0
+	}
+
+	public getAuthorName() {
+		return author.getName();
+	}
+
+	public getAuthorID() {
+		return author.getID();
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public double getRank() {
+		return ranking;
+	}
+
+	public void voteRank(Boolean positive) {
+		if (positive) {
+			ranking += 1.0;
+		}
+		else {
+			ranking -= 1.0;
+		}
 	}
 }
