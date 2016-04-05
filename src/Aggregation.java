59d010c73cb1872d0ce87371f55e87c9adee9786
annawuyang <annayang@wharton.upenn.edu>
2016-04-04 22:07:26 -0400

    Aggregation

--- src/Aggregation.java
+++ src/Aggregation.java
@@ -1,5 +1,29 @@
 public class Aggregation {
-	
+	Professor p;
+	HashSet<Review> reviews;
 
-	
+	public Aggregation(Professor p) {
+		this.p = p;
+		this.reviews = p.getReviews();
+		
+	}
+
+	public calcScores() {
+		double totalRank = 0;
+		double finalDifficulty = 0; 
+		double finalHelpfulness = 0;
+		double finalEngagement = 0; 
+		double finalOverall = 0; 
+		double userDifficulty = 0; 
+		double userHelpfulness = 0;
+		double userEngagement = 0;
+		double userOverall =0;
+		for (int r = 0; r < reviews.size(); r++) {
+			User currentUser = r.getUser();
+			totalRank += currentUser.getRank();
+		}
+
+	}
+
+
 }
