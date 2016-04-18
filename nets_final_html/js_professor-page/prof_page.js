39ba42612f8584296f1cfe179bd6e75994d542f5
Sonia <Sonia@vagvlan536.179.wlan.library.upenn.edu>
2016-04-18 16:20:24 -0400

    new firebase link

--- nets_final_html/js_professor-page/prof_page.js
+++ nets_final_html/js_professor-page/prof_page.js
@@ -10,7 +10,7 @@ function pull_info(){
 		 final_prof_name = tmp.concat(adjusted_prof_name[1]);
 		 
 	}
-	
+
 	else if (adjusted_prof_name.length > 2) {
 		//TODO: Throw error if prof name is of length 3 or greater
 	}
@@ -18,32 +18,39 @@ function pull_info(){
 	// var adjusted_prof_name; //figure out how to remove spaces and replace with %20	
 
 	
-	// var prof_ref = new Firebase(("https://penn-prof-review.firebaseio.com/Professor/").concat(final_prof_name));
+	var prof_link = "https://penn-prof-review.firebaseio.com/Professor/" + final_prof_name;
+
+	var prof_ref = new Firebase(prof_link);
+		// .concat(final_prof_name));
 	
-	// window.alert(final_prof_name);
+	
 
-	// prof_ref.on("child_added", function(snapshot, prevChildKey) {
-	// 	var newPost = snapshot.val();
- //  		console.log("Review: " + newPost.review_id;
- //  		window.alert(newPost.review_id);
-	// });
+	prof_ref.on("child_added", function(snapshot, prevChildKey) {
+		var newPost = snapshot.val();
+  		// console.log("Review: " + newPost.review_id;
+  		window.alert(newPost.review_id);
+	});
 
-// // 	ref.on("value", function(snapshot) {
-// //   console.log(snapshot.val());
-// //   window.alert(snapshot.val());
-// // }, function (errorObject) {
-// //   console.log("The read failed: " + errorObject.code);
-// // });
+	window.alert("review");
 
-// // 	prof_ref.once("value", function(snapshot) {
-// //   // The callback function will only get called once since we return true
-// //   snapshot.forEach(function(childSnapshot) {
-// //     var key = childSnapshot.key();
-// //     // key === "fred"
-// //     return true;
-// //     window.alert(snapshot.val());
-// //   });
-// // });
+// 	window.alert(final_prof_name);
+
+// 	prof_ref.on("value", function(snapshot) {
+//   console.log(snapshot.val());
+//   window.alert(snapshot.val());
+// }, function (errorObject) {
+//   console.log("The read failed: " + errorObject.code);
+// });
+
+// 	prof_ref.once("value", function(snapshot) {
+//   // The callback function will only get called once since we return true
+//   snapshot.forEach(function(childSnapshot) {
+//     var key = childSnapshot.key();
+//     // key === "fred"
+//     return true;
+//     window.alert(snapshot.val());
+//   });
+// });
 // 	var num_reviews = 
 
 // 	// if num_reviews == 0 or prof doesn't exist
