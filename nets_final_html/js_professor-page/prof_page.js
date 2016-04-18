7a0e6c4e36e7e7a4e67c24d071200a2ab4dad3d2
niveditasankar <dita.sankar@gmail.com>
2016-04-18 14:05:14 -0400

    prof_page js edits

--- nets_final_html/js_professor-page/prof_page.js
+++ nets_final_html/js_professor-page/prof_page.js
@@ -2,10 +2,22 @@ function pull_info(){
 
 
 	var prof_name = document.getElementById("search_form").value;
-
-	var adjusted prof_name; //figure out how to remove spaces and replace with %20	
 	
-	var prof_ref = new Firebase(("https://penn-prof-review.firebaseio.com/Professor/").concat(adjusted_prof_name));
+	var adjusted_prof_name = prof_name.split(" ");
+	
+	var final_prof_name;
+	if (adjusted_prof_name.length ==2) {
+		 var tmp = adjusted_prof_name[0].concat("%20");
+		 final_prof_name = tmp.concat(adjusted_prof_name);
+		 
+	}
+	else if (adjusted_prof_name.length > 2) {
+		//TODO: Throw error if prof name is of length 3 or greater
+	}
+
+	var adjusted_prof_name; //figure out how to remove spaces and replace with %20	
+	
+	var prof_ref = new Firebase(("https://penn-prof-review.firebaseio.com/Professor/").concat(final_prof_name));
 
 	var num_reviews = 
 
