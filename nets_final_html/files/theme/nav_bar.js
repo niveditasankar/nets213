7cf4829f6e0febd37cee366abedd73bc264dd122
zhusteph <zhusteph@wharton.upenn.edu>
2016-04-18 03:05:55 -0400

    log out functionality

--- nets_final_html/files/theme/nav_bar.js
+++ nets_final_html/files/theme/nav_bar.js
@@ -4,17 +4,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	if (authData) {
 	  document.getElementById("loginId").innerHTML="Logout";
 	} else {
-	  document.getElementById("loginId").innerHTML="Logout";
+	  document.getElementById("loginId").innerHTML="Login";
 	}
 });
 
-// function authenticate() {
-//     var ref = new Firebase("https://penn-prof-review.firebaseio.com");
-// 	var authData = ref.getAuth();
-// 	if (authData) {
-// 	  document.getElementById("loginId").innerHTML="Logout";
-// 	} else {
-// 	  document.getElementById("loginId").innerHTML="Logout";
-// 	}
-// };
+function logout() {
+    var ref = new Firebase("https://penn-prof-review.firebaseio.com");
+	var authData = ref.getAuth();
+	if (authData && document.getElementById("loginId").innerHTML=="Logout") {
+		ref.unauth();
+		document.getElementById("loginId").innerHTML="Login";
+	}
+};
 
